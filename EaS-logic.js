c013011e1052d17ca28586f4db11507817203b9e
Marc San <sanchezpe.marc@gmail.com>
2025-07-21 13:21:48 +0200

    Solucionar errores grid
    Se solucionan errores de la grid y se modifica el estilo dándole
    la dinámica deseada.

--- EaS-logic.js
+++ EaS-logic.js
@@ -1,16 +1,18 @@
+// Se crea selector del cuerpo del documento.
 const body = document.querySelector("body"); 
 console.log(body)
+
+// Se crea contenedor de todos los cuadrados y se añade clase
 const squares = document.createElement("div");
 squares.classList.add("squares");
-squares.setAttribute("style", "border: solid black 2px");
 
-
-for (let i = 0; i <= 16*16; i++) {
+// Bucle que crea cada uno de los cuadrados, se añade clase
+// y los inserta en el contenedor principal
+for (let i = 0; i < 16*16; i++) {
     const square = document.createElement("div");
     square.classList.add("square");
-    square.setAttribute("style", "border: solid black 1px, background-color: white");
     squares.appendChild(square)
 }
 
-
+// Se inserta contenedor principal en el cuerpo
 body.appendChild(squares);
